37c5fb618e89be9e6cc51f721447ce7a77bbbf02
Chey Rotana <rotana.chey@student.cadt.edu.kh>
2026-02-06 09:56:31 +0700

    BLA-001 - Rides Service - Filter rides

--- lib/services/rides_service.dart
+++ lib/services/rides_service.dart
@@ -1,3 +1,5 @@
+import 'package:blabla/model/ride/locations.dart';
+
 import '../data/dummy_data.dart';
 import '../model/ride/ride.dart';
 
@@ -9,4 +11,43 @@ class RidesService {
   static List<Ride> allRides = fakeRides;
 
 
+  static List<Ride> filterByDeparture(Location departure) {
+    List<Ride> rideByDeparture = allRides
+        .where((ride) => ride.departureLocation == departure)
+        .toList();
+    return rideByDeparture;
+  }
+
+  static List<Ride> filterBySeatRequested(int seatRequested) {
+    List<Ride> rideBySeatRequested = allRides
+        .where((ride) => ride.availableSeats == seatRequested)
+        .toList();
+    return rideBySeatRequested;
+  }
+
+  static List<Ride> filterBy({Location? departure, int? seatRequested}) {
+
+    if(departure == null) {
+      List<Ride> rideBySeatRequested = allRides
+          .where((ride) => ride.availableSeats == seatRequested)
+          .toList();
+      return rideBySeatRequested;
+    } 
+    else if (seatRequested == null) {
+          List<Ride> rideByDeparture = allRides
+          .where((ride) => ride.departureLocation == departure)
+          .toList();
+      return rideByDeparture;
+    }
+    else {
+      List<Ride> rideByDepartureSeatRequested = allRides
+        .where(
+          (r) =>
+              r.departureLocation == departure &&
+              r.availableSeats == seatRequested,
+        )
+        .toList();
+      return rideByDepartureSeatRequested;
+    }
+  }
 }
--- test/test_rides.dart
+++ test/test_rides.dart
@@ -4,11 +4,8 @@ import 'package:blabla/model/ride/locations.dart';
 import 'package:blabla/services/rides_service.dart';
 
 void main() {
-  Location dijon = Location(country: Country.france, name: "Dijon");
-
-  List<Ride> filteredRide = RidesService.filter(requestedSeats: 2, departureLocation: dijon);
-
-  for (Ride ride in filteredRide) {
-    print(ride);
-  }
+  RidesService.filterBy(
+    departure: Location(name: "Dijon", country: Country.france),
+    seatRequested: 2,
+  );
 }
